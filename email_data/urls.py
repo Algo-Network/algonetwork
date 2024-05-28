ab64667825a71043db78f080b3f1914f4a63d58a
dipa.alhaza <dipa.alhaza@ui.ac.id>
2024-05-28 13:25:37 +0700

    fix: fix user interface

--- email_data/urls.py
+++ email_data/urls.py
@@ -6,7 +6,6 @@ app_name = 'email_data'
 urlpatterns = [
     path('', views.email_list, name='email_list'),
     path('email/new/', views.email_create, name='email_create'),  # Pastikan URL pattern untuk create ada di sini
-    path('email/<str:email>/', views.email_detail, name='email_detail'),
     path('email/<str:email>/edit/', views.email_update, name='email_update'),
     path('email/<str:email>/delete/', views.email_delete, name='email_delete'),
     path('api/emails/', views.get_all_emails, name='get_all_emails'),
