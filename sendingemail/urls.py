bc40aa57b71b609a70324d1bdf98a3fa32160de2
dipa.alhaza <dipa.alhaza@ui.ac.id>
2024-06-11 17:06:20 +0700

    fix: fix bug

--- sendingemail/urls.py
+++ sendingemail/urls.py
@@ -1,10 +1,8 @@
 # urls.py
 from django.urls import path
-from .views import schedule_email_post, schedule_success_view
-
+from .views import schedule_email_post
 app_name = 'sendingemail'
 
 urlpatterns = [
     path('schedule-email/', schedule_email_post, name='schedule_email'),
-    path('schedule-success/', schedule_success_view, name='schedule_success'),
 ]
